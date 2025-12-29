ee712c28efa55276f1690ce6a18c3082c37b1307
Hayelom Kiros <hayelomkiros20@gmail.com>
2025-12-29 20:53:47 +0300

    creating window for basic cross

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 #include "config.h"
 #include "utils.h"
 
-// we are going to draw a rectangle with  Element Buffer Object (EBO)
-// this is more efficient than using two triangles when the number of vertices increases
-
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -11,23 +8,26 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 
 int main()
 {
+    std::cout << "HELLO GOD!!!" << std::endl;
+
     glfwInit();
 
-    // set window config to 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                 // 3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                 //.3
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // core the latest specification
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for mac devices
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Opengl Test", nullptr, nullptr);
-    if (!window)
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
@@ -37,63 +37,11 @@ int main()
         glfwTerminate();
         return -1;
     }
-
-    std::string vertexShaderPath = "../shaders/triangles/triangle.vert";
-    std::string fragmentShaderPath = "../shaders/triangles/triangle.frag";
-
-    std::string vertexCode = loadShaderSourceFromFile(vertexShaderPath);
-    std::string fragmentCode = loadShaderSourceFromFile(fragmentShaderPath);
-
-    unsigned int program = createShader(
-        vertexCode,
-        fragmentCode);
-
-    float vertices[] = {
-        0.5f, 0.5f, 0.0f,   // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f   // top left
-    };
-    unsigned int indices[] = {
-        // note that we start from 0!
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
-    };
-
-    // load data to buffer
-    unsigned int VBO, VAO, EBO;
-    // generate buffer ids
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO); // element buffer object for index data
-    glGenVertexArrays(1, &VAO);
-
-    // 1. bind Vertex Array Object
-    glBindVertexArray(VAO);
-
-    // 2. copy our vertices array in a vertex buffer for OpenGL to use
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);                                        // selected buffer vbo
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // copy data to buffer vbo
-
-    // 3. copy our index array in a element buffer for OpenGL to use
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // 4. then set the vertex attributes pointers
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-
-    // select EBO
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
-
-        glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(program);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        glfwSwapBuffers(window);
     }
+
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
