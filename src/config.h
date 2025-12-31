41f8bdc4081cd2f3811b9e9406ccd1e5130f20d1
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-01-01 00:01:08 +0300

    making it 3d

--- src/config.h
+++ src/config.h
@@ -3,6 +3,7 @@
 // Configuration header for the project
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
