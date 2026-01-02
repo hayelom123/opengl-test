87343d6e5ab0493730ac6e39fc867acd2e01ace9
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-01-02 15:43:57 +0300

    loading texture from file

---
+++ src/stb_image.cpp
@@ -0,0 +1,5 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// for loading image as texture
+// You can add more configuration settings or utility function declarations here as needed
