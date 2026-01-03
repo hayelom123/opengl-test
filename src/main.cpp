717ccd183033dce409f40b704d1651dac67c81aa
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-01-03 15:56:50 +0300

    changing and loading texture

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ int main()
 
     // load texture data
     int width, height, nrChannels;
-    unsigned char *data = stbi_load("./container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("./textures/wall.jpg", &width, &height, &nrChannels, 0);
     if (!data)
     {
         std::cout << "Failed to load texture" << std::endl;
