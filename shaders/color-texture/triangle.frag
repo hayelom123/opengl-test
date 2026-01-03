7fba2fd3422fdff075facb7b79b24ffd47f22b52
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-01-03 16:57:30 +0300

    using both texture and color

--- shaders/color-texture/triangle.frag
+++ shaders/color-texture/triangle.frag
@@ -10,5 +10,9 @@ uniform sampler2D ourTexture;
 
 void main()
 {
-   FragColor = texture(ourTexture, TexCoord);
+FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);
+//To get a little funky we can also mix the resulting texture color with the vertex colors. 
+//We simply multiply the resulting texture color with the vertex color in the fragment shader to mix both colors:
+
+  
 }
--- src/main.cpp
+++ src/main.cpp
@@ -44,8 +44,8 @@ int main()
     }
 
     // shader compilation
-    std::string vertexShaderPath = "../shaders/texture/triangle.vert";
-    std::string fragmentShaderPath = "../shaders/texture/triangle.frag";
+    std::string vertexShaderPath = "../shaders/color-texture/triangle.vert";
+    std::string fragmentShaderPath = "../shaders/color-texture/triangle.frag";
 
     std::string vertextShaderSource = loadShaderSourceFromFile(vertexShaderPath);
     std::string fragmentShaderSource = loadShaderSourceFromFile(fragmentShaderPath);
@@ -60,40 +60,50 @@ int main()
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
 
-    // ---------- VERTEX DATA ----------
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
     float vertices[] = {
-        // positions   /*================*///tex coords
-        0.5f, 0.5f, /*================*/1.0f, 1.0f,
-        0.5f, -0.5f, /*================*/1.0f, 0.0f,
-        -0.5f, -0.5f, /*================*/0.0f, 0.0f,
+        // positions                 // colors           // texture coords
+        0.5f, 0.5f, 0.0f, /*------*/ 1.0f, 0.0f, 0.0f, /*------*/ 1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 0.0f, /*------*/ 0.0f, 1.0f, 0.0f, /*------*/ 1.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, /*------*/ 0.0f, 0.0f, 1.0f, /*------*/ 0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, /*------*/ 1.0f, 1.0f, 0.0f, /*------*/ 0.0f, 1.0f   // top left
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
 
-        -0.5f, -0.5f, /*================*/0.0f, 0.0f,
-        -0.5f, 0.5f, /*================*/0.0f, 1.0f,
-        0.5f, 0.5f, /*================*/1.0f, 1.0f};
-
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // position attribute
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
-    // stride is 4 floats (2 position, 2 texture coordinates)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    // stride is 8 floats (3 position,3 colors, 2 texture coordinates)
     // simply put, it is the space between consecutive vertex attributes
     // the pointer is offset of the first component
 
-    // texture coordinate attribute
+    // color attribute
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
 
+    // texture coordinate attribute
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
     // load texture data
     int width, height, nrChannels;
-    unsigned char *data = stbi_load("./textures/wall.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("./textures/container.jpg", &width, &height, &nrChannels, 0);
     if (!data)
     {
         std::cout << "Failed to load texture" << std::endl;
@@ -109,8 +119,8 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method). t axis means y axis of texture
 
     // filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // when texture is scaled down
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); // when texture is scaled up
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); // when texture is scaled down
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);              // when texture is scaled up
 
     // load texture data
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -118,7 +128,7 @@ int main()
 
     //
     glUseProgram(shaderProgram);
-    glUniform1i(glGetUniformLocation(shaderProgram, "checkerTex"), 0); // set the texture unit 0 to the sampler2D uniform
+    glUniform1i(glGetUniformLocation(shaderProgram, "ourTexture"), 0); // set the texture unit 0 to the sampler2D uniform
     // glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     // render loop
     while (!glfwWindowShouldClose(window))
@@ -138,7 +148,7 @@ int main()
 
         // render the triangle
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
